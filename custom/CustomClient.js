8ca728a5fee610f68a64deba8b69bae3f841f9f2
Nunes <nunestech2@gmail.com>
2025-03-09 21:18:34 -0300

    CustomClient criado

--- custom/CustomClient.js
+++ custom/CustomClient.js
@@ -10,10 +10,28 @@ module.exports = class CustomClient extends Client {
 
     async removeMember(idGroup, idMember) {
         try {
+            if(!idGroup.endsWith('@g.us')) {
+                return false
+            }
             const chat = await super.getChatById(idGroup);
             const groupChat = new GroupChat(this, chat)
 
-            await groupChat.removeParticipants([idMember]);
+            await groupChat.removeParticipant([idMember]);
+            return true;
+        } catch(error) {
+            return false;
+        };
+    };
+
+    async addMember(idGroup, idMember) {
+        if(!idGroup.endsWith('@g.us')) {
+            return false
+        }
+        try {
+            const chat = await super.getChatById(idGroup);
+            const groupChat = new GroupChat(this, chat)
+
+            await groupChat.addParticipants(idMember)
             return true;
         } catch(error) {
             return false;
